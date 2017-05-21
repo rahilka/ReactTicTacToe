b7f388d36d9201396feec9a3ead46a9f584ecb59
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-05-21 23:11:07 +0200

    Calculate winner

--- index.js
+++ index.js
@@ -19,6 +19,27 @@ function Square (props) {
   );
 }
 
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
 class Board extends React.Component {
 
   // When you want to aggregate data from multiple children or to have two child components communicate with each other,
@@ -39,6 +60,11 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice(); // copy the squares array prior to making changes and to prevent mutating the existing array
+
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -55,9 +81,14 @@ class Board extends React.Component {
     );
   }
 
-
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
