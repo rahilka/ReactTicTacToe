ec0051cff7af8ca2e2e957a997a494ddc7755be7
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-05-21 22:14:25 +0200

    Added state

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  // store the current value of the square in state,
+  // and change it when the sqare is clicked
+
+  constructor(props) {
+    super();  //In JavaScript classes, you need to explicitly call super(); when defining the constructor of a subclass
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      // Whenever this.setState is called, an update to the component is scheduled
+      // causing React to merge in the passed state update and rerender the component along with its descendants.
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
@@ -14,7 +26,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
