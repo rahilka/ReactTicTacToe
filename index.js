f16c7d5085edd6db22211e5b1634a502fe28a444
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-05-21 22:37:12 +0200

    Remove state to Board component

--- index.js
+++ index.js
@@ -6,28 +6,60 @@ class Square extends React.Component {
   // store the current value of the square in state,
   // and change it when the sqare is clicked
 
-  constructor(props) {
-    super();  //In JavaScript classes, you need to explicitly call super(); when defining the constructor of a subclass
-    this.state = {
-      value: null,
-    };
-  }
+  // constructor() {
+  //   super();  //In JavaScript classes, you need to explicitly call super(); when defining the constructor of a subclass
+  //   this.state = {
+  //     value: null,
+  //   };
+  // }
+
+  // Square no longer keeps its own state;
+  // it receives its value from its parent Board and informs its parent when it's clicked.
+  // We call components like this controlled components.
 
   render() {
     return (
       // Whenever this.setState is called, an update to the component is scheduled
       // causing React to merge in the passed state update and rerender the component along with its descendants.
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+
+  // When you want to aggregate data from multiple children or to have two child components communicate with each other,
+  // move the state upwards so that it lives in the parent component.
+  // The parent can then pass the state back down to the children via props,
+  // so that the child components are always in sync with each other and with the parent.
+
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
+
+  // To update the state of Board,
+  // The usual pattern is pass down a function from Board to Square that gets called when the square is clicked.
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+       />  //we pass two props from Board to Square: value and onCLick
+    );
+  }
+
 
   render() {
     const status = 'Next player: X';
